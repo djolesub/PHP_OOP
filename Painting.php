4b5e7478d00b499728f033ac08d4586306f863b7
djordjesubotic <djolesub@gmail.com>
2016-09-18 18:55:23 +0200

    Finishing Painting class.Problem with child toString method because of table

--- Painting.php
+++ Painting.php
@@ -7,6 +7,32 @@
  * Time: 6:43 PM
  */
 class Painting{
+    //Defining fields
+    private $medium;
+
+    //Defining Constructor
+    private function __construct($name,$artist,$yearCreated,$medium){
+        parent::__construct($name,$artist,$yearCreated);
+        $this->setMedium();
+    }
+
+    //Definig getters
+    public function getMedium(){return $this->medium;}
+
+    //Defining Setters
+    public function setMedium($medium){
+        if(is_string($medium)){
+            $this->medium = $medium;
+        }else {
+            echo "Wrong input for medium";
+        }
+    }
+
+    //Problem with toString because parent toString creates and return html table.
+    //What to do do now with medium because parents toString has </table> closing tag
+    public function __toString(){
+        return parent::__toString() ."<br>Medium is: ".$this->medium;
+    }
 
 
 
